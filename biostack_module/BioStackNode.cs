4638c685ac7c7592c722d74541149b016d8169f1
Ryan D. Lewis <public+github@ryandlewis.dev>
2024-04-05 14:59:06 -0500

    Initial commit

---
+++ biostack_module/BioStackNode.cs
@@ -0,0 +1,116 @@
+﻿using Grapevine;
+using McMaster.Extensions.CommandLineUtils;
+using BTIAUTOSTACKERLib;
+
+namespace biostack_module
+{
+    class BioStackNode
+    {
+
+        public static int Main(string[] args) => CommandLineApplication.Execute<BioStackNode>(args);
+
+        [Option(Description = "Server Hostname")]
+        public string Hostname { get; set; } = "+";
+
+        [Option(Description = "Server Port")]
+        public int Port { get; } = 2000;
+
+        [Option(Description = "Whether or not to simulate the instrument")]
+        public bool Simulate { get; } = true;
+
+        public string state = ModuleStatus.INIT;
+        private IRestServer server;
+        private BTIAutoStacker bTIAutoStacker = new BTIAutoStacker();
+        private short stackerComPort = 5;
+
+
+        public void deconstruct()
+        {
+            Console.WriteLine("Exiting...");
+            server.Stop();
+            if (!Simulate)
+            {
+                bTIAutoStacker.CloseComPort();
+            }
+            Console.WriteLine("Exited...");
+        }
+
+        private void OnExecute()
+        {
+            InitializePlateStacker();
+
+            server = RestServerBuilder.UseDefaults().Build();
+            string server_url = "http://" + Hostname + ":" + Port.ToString() + "/";
+            Console.WriteLine(server_url);
+            server.Prefixes.Clear();
+            server.Prefixes.Add(server_url);
+            server.Locals.TryAdd("state", state);
+            try
+            {
+                //server.Start();
+                Console.WriteLine("Press enter to stop the server");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                deconstruct();
+            }
+        }
+
+        private void InitializePlateStacker()
+        {
+            if (Simulate)
+            {
+                bTIAutoStacker.EnableSimulation(1);
+            }
+            else
+            {
+                bTIAutoStacker.SetComPort(stackerComPort);
+                bTIAutoStacker.OpenComPort(stackerComPort);
+            }
+            bTIAutoStacker.ActionIsCompleted += StackerActionCompleteHandler;
+            Console.Write("Communications Test (1 means OK): ");
+            Console.WriteLine(bTIAutoStacker.TestCommunicationWithoutDialog());
+            PrintResponse(bTIAutoStacker.IdentifyConfiguredInstrument(8));
+            PrintSystemStatus();
+            state = ModuleStatus.BUSY;
+            PrintResponse(bTIAutoStacker.HomeAllAxes());
+            while (state == ModuleStatus.BUSY)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void StackerActionCompleteHandler(short nMessageObject, short nReturnCode)
+        {
+            Console.WriteLine("---------------");
+            Console.WriteLine("ACTION COMPLETE");
+            PrintResponse(nMessageObject);
+            PrintResponse(nReturnCode);
+            Console.WriteLine("---------------");
+            state = ModuleStatus.IDLE;
+        }
+
+        public void PrintResponse(short response_code)
+        {
+            if (response_code > 1)
+            {
+                Console.WriteLine(response_code.ToString("X4"));
+            }
+            else
+            {
+                Console.WriteLine(response_code.ToString());
+            }
+        }
+
+        public void PrintSystemStatus()
+        {
+            Console.Write("System Status: ");
+            PrintResponse(bTIAutoStacker.GetSystemStatus());
+        }
+    }
+}
